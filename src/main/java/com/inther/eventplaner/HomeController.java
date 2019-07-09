6dd038d54988b395302f829efe00cf71674aa5c2
Sergiu <uigres2010@gmail.com>
2019-07-09 10:55:49 +0300

    Controller

---
+++ src/main/java/com/inther/eventplaner/HomeController.java
@@ -0,0 +1,15 @@
+package com.inther.eventplaner;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello Spring Boot";
+
+    }
+}
