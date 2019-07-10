07f658e3e93240b7a0e0a8db0c5213e8c549b877
Fortuna Mircea <itsmynamenoname@gmail.com>
2019-07-10 10:28:10 +0300

    Hibernate config

---
+++ src/main/java/com/inther/eventplaner/models/RegistrationDTO.java
@@ -0,0 +1,39 @@
+//package com.inther.eventplaner.models;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table (name="users")
+//public class RegistrationDTO {
+//    @Id
+//    @GeneratedValue
+//    int id;
+//    @Column(name = "username")
+//    String username;
+//    @Column(name="password")
+//    String password;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//}
