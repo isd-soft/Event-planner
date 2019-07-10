b5ae6f2e36863b537dfb9c70f73e148100be2ee8
uigre <uigres2010@gmail.com>
2019-07-10 17:15:11 +0300

    Login autorisation in my branch

--- src/main/java/com/inther/eventplaner/controller/MainController.java
+++ src/main/java/com/inther/eventplaner/controller/MainController.java
@@ -5,14 +5,12 @@ import com.inther.eventplaner.domain.User;
 import com.inther.eventplaner.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/demo")
 public class MainController {
-    @GetMapping("/")
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting "+name;
--- src/main/java/com/inther/eventplaner/controller/RegistrationController.java
+++
@@ -1,4 +0,0 @@
-package com.inther.eventplaner.controller;
-
-public class RegistrationController {
-}
