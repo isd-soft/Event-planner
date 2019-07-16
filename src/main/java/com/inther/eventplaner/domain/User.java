90650c86dc26376eb54b41dd7e2d4ef8e247d548
Nicolai Vilciu <nickvilciu@gmail.com>
2019-07-16 15:34:51 +0300

    Add Event Model and Repository Minor changes in JWTAuthentificationController
    Signed-off-by: Nicolai Vilciu <nickvilciu@gmail.com>

--- src/main/java/com/inther/eventplaner/domain/User.java
+++ src/main/java/com/inther/eventplaner/domain/User.java
@@ -1,6 +1,5 @@
 package com.inther.eventplaner.domain;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,29 +11,18 @@ import java.util.Set;
 public class User  {
 
     public User() {
+    }
 
+    public User(String username, String password){
+        this.username=username;
+        this.password=password;
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column(name = "firstname", nullable = false)
-    private String firstname;
-    @Column(name = "secondname", nullable = false)
-    private String secondname;
-
-    @Column(name = "username", nullable = false)
     private String username;
-
-    @Column(name = "email", nullable = false)
-    private String email;
-
-    @Column(name = "phonenuber", nullable = true)
-    private String phonenumber;
-    @Column(name = "photo", nullable = true)
-    private String photo;
-    @Column(name = "password", nullable = false)
     private String password;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
@@ -51,55 +39,10 @@ public class User  {
         this.id = id;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getSecondname() {
-        return secondname;
-    }
-
-    public void setSecondname(String secondname) {
-        this.secondname = secondname;
-    }
-
     public String getUsername() {
         return username;
     }
 
-
-
-
-
-
-
-    public String getPhonenumber() {
-        return phonenumber;
-    }
-
-    public void setPhonenumber(String phonenumber) {
-        this.phonenumber = phonenumber;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
     public void setUsername(String username) {
         this.username = username;
     }
--- src/main/java/com/inther/eventplaner/service/JwtUserDetailsService.java
+++ src/main/java/com/inther/eventplaner/service/JwtUserDetailsService.java
@@ -1,7 +1,5 @@
 package com.inther.eventplaner.service;
 
-import java.util.ArrayList;
-
 import com.inther.eventplaner.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +8,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
@@ -20,7 +20,7 @@ public class JwtUserDetailsService implements UserDetailsService {
     @Autowired
     private PasswordEncoder bcryptEncoder;
 
-    @Override
+    /*@Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         // HARDCODE CHECK FOR USER
@@ -30,21 +30,26 @@ public class JwtUserDetailsService implements UserDetailsService {
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
+    }*/
 
-//        User user = userRepository.findByUsername(username);
-//        if (user == null) {
-//            throw new UsernameNotFoundException("User not found with username: " + username);
-//        }
-//        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-//                new ArrayList<>());
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        com.inther.eventplaner.domain.User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
     }
 
-//    public User save(User newUser) {
-//        newUser = new User();
-//        newUser.setUsername(newUser.getUsername());
-//        newUser.setPassword(bcryptEncoder.encode(newUser.getPassword()));
-//        return userRepository.save(newUser);
-//    }
+    /*public UserRepository save(User user) {
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        userRepository.save(newUser);
+        return (UserRepository) userRepository.save(user);
+    }*/
 
 
 
