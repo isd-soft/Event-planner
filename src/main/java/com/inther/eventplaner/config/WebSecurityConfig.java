53fafe7c6b4b87d0f41f756522c3cd6f56b16c07
uigre <uigres2010@gmail.com>
2019-07-16 09:44:44 +0300

    Registration

--- src/main/java/com/inther/eventplaner/config/WebSecurityConfig.java
+++ src/main/java/com/inther/eventplaner/config/WebSecurityConfig.java
@@ -1,20 +1,28 @@
 package com.inther.eventplaner.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -26,16 +34,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().disable();
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("u")
-                        .password("p")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .usersByUsernameQuery("select username, password from users where username=?");
     }
+
+
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("u")
+//                        .password("p")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
--- src/main/java/com/inther/eventplaner/controller/GreetingController.java
+++ src/main/java/com/inther/eventplaner/controller/GreetingController.java
@@ -1,44 +1,32 @@
 package com.inther.eventplaner.controller;
+
 import com.inther.eventplaner.domain.User;
 import com.inther.eventplaner.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Method;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class GreetingController {
     @Autowired
     private UserRepository userRepository;
 
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting" ;
-    }
-
-
-
-//    @GetMapping("/login")
     @PostMapping("/login")
-//@RequestMapping(value = "/login", method = RequestMethod.POST)
+
     public String greeting(@RequestParam String username
             , @RequestParam String password, Model model) {
-//        model.addAttribute("name", name);
+
         return password;
     }
 
 
-    @PostMapping(path="/add") // Map ONLY GET Requests
-    public @ResponseBody String addNewUser (@RequestParam String username
+    @PostMapping(path = "/add")
+    public  String addNewUser(@RequestParam String username
             , @RequestParam String password, Model model) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
@@ -47,34 +35,35 @@ public class GreetingController {
         n.setUsername(username);
         n.setPassword(password);
         userRepository.save(n);
-//        model.addAttribute("name", username);
-//        return "content"+ username;
-        return "Saved user is "+ n.getUsername()+" "+ n.getPassword();
+        return "Saved user is " + n.getUsername() + " " + n.getPassword();
     }
 
-    @GetMapping(path="/all")
+    @GetMapping(path = "/all")
     public Iterable<User> findAll(Map<String, Object> model) {
         // This returns a JSON or XML with the users
-        Iterable<User> user=userRepository.findAll();
+        Iterable<User> user = userRepository.findAll();
 
 
-        model.put("utilizatori",user );
+        model.put("utilizatori", user);
         return user;
     }
 
-    @PostMapping(path="/authenticate")
-    public String authenticationMeth(@RequestParam String username,@RequestParam String password) {
+    @PostMapping(path = "/authenticate")
+    public String authenticationMeth(@RequestParam String username, @RequestParam String password) {
         // This returns a JSON or XML with the users
 
-      if( userRepository.findByUsername(username).getUsername().equals(username)
-              &&userRepository.findByUsername(username).getPassword().equals(password)){
+        if (userRepository.findByUsername(username).getUsername().equals(username)
+               && userRepository.findByUsername(username).getPassword().equals(password) ) {
+
+return "is valid. User name is:"+userRepository.findByUsername(username).getUsername()+" .Password is:"
+        +userRepository.findByUsername(username).getPassword();
+
+        }else{
+            return "no such user name";
+
+        }
 
 
-
-
-
-      };
-        return "pass";
     }
 
 
