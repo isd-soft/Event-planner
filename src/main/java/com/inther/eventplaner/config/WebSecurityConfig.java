fe69f8d0b6dcf456553c652aa87d45efb92970ce
Nicolai Vilciu <nickvilciu@gmail.com>
2019-07-16 10:03:11 +0300

    Add JWT
    Signed-off-by: Nicolai Vilciu <nickvilciu@gmail.com>

--- src/main/java/com/inther/eventplaner/config/WebSecurityConfig.java
+++ src/main/java/com/inther/eventplaner/config/WebSecurityConfig.java
@@ -54,16 +54,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 
-/*
-        httpSecurity
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/login", "/all", "/add", "/authenticate").permitAll()
-                .anyRequest().authenticated().and()
-                .formLogin().disable();
-*/
-
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
---
+++ src/main/java/com/inther/eventplaner/service/JwtUserDetailsService.java
@@ -0,0 +1,53 @@
+package com.inther.eventplaner.service;
+
+import java.util.ArrayList;
+
+import com.inther.eventplaner.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.inther.eventplaner.domain.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // HARDCODE CHECK FOR USER
+        /*if ("javainuse".equals(username)) {
+            return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }*/
+
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public User save(User newUser) {
+        newUser = new User();
+        newUser.setUsername(newUser.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(newUser.getPassword()));
+        return userRepository.save(newUser);
+    }
+
+
+
+
+
+}
