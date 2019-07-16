b037b646198e7bebd34d0b2a6bc227b5ea66f8e4
Nicolai Vilciu <nickvilciu@gmail.com>
2019-07-16 17:53:43 +0300

    Merge branches 'master' and 'nvilciu' of https://github.com/isd-soft/event-planner into nvilciu
    Signed-off-by: Nicolai Vilciu <nickvilciu@gmail.com>
    
    # Conflicts:
    #	src/main/java/com/inther/eventplaner/controller/GreetingController.java
    #	src/main/java/com/inther/eventplaner/controller/JwtAuthenticationController.java
    #	src/main/java/com/inther/eventplaner/domain/User.java
    #	src/main/java/com/inther/eventplaner/service/JwtUserDetailsService.java

---
+++ src/main/java/com/inther/eventplaner/controller/EventController.java
@@ -0,0 +1,59 @@
+package com.inther.eventplaner.controller;
+
+import com.inther.eventplaner.domain.Event;
+import com.inther.eventplaner.exception.ResourceNotFoundException;
+import com.inther.eventplaner.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+public class EventController {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @GetMapping("/events")
+    public Page<Event> getAllEvents(Pageable pageable) {
+        return eventRepository.findAll(pageable);
+    }
+
+    @GetMapping("/events/{eventId}")
+    public Optional<Event> getEvent(@PathVariable Integer eventId) {
+        return eventRepository.findById(eventId);
+    }
+
+    @PostMapping("/events")
+    public Event createEvent(@Valid @RequestBody Event event) {
+        return eventRepository.save(event);
+    }
+
+    @PutMapping("/events/{eventId}")
+    public Event updateEvent(@PathVariable Integer eventId, @Valid @RequestBody Event eventRequest) {
+        return eventRepository.findById(eventId).map(event -> {
+            event.setTitle(eventRequest.getTitle());
+            event.setDescription(eventRequest.getDescription());
+            event.setCategory(eventRequest.getCategory());
+            event.setDate(eventRequest.getDate());
+            event.setPrice(eventRequest.getPrice());
+            event.setLocation(eventRequest.getLocation());
+            return eventRepository.save(event);
+        }).orElseThrow(() -> new ResourceNotFoundException("EventId " + eventId + " not found"));
+    }
+
+
+    @DeleteMapping("/events/{eventId}")
+    public ResponseEntity<?> deleteEvent(@PathVariable Integer eventId) {
+        return eventRepository.findById(eventId).map(event -> {
+            eventRepository.delete(event);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("EventId " + eventId + " not found"));
+    }
+
+
+}
